5a7dcd9112af9464f1f3fcd53aef82ea3d4994e0
akhand08 <adib08@outlook.com>
2025-08-18 22:50:42 +0600

    chapter 1 done of implementing streaming of data

--- main.go
+++ main.go
@@ -3,9 +3,55 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+
+		defer file.Close()
+		defer close(out)
+
+		str := ""
+
+		for {
+			data := make([]byte, 8)
+
+			n, err := file.Read(data)
+
+			if err != nil {
+
+				if len(str) > 0 {
+					out <- str
+					out <- "end"
+				}
+
+				if err != io.EOF {
+					log.Fatal("error: ", err)
+				}
+
+				break
+			}
+
+			if newLineIndex := bytes.IndexByte(data, '\n'); newLineIndex != -1 {
+				str += string(data[:newLineIndex])
+				// fmt.Printf("read: %s\n", str)
+				out <- str
+				str = string(data[newLineIndex+1:])
+			} else {
+				str += string(data[:n])
+			}
+
+		}
+
+	}()
+
+	return out
+}
 
 func main() {
 
@@ -14,36 +60,12 @@ func main() {
 		log.Fatal("error", err)
 	}
 
-	str := ""
+	lines := getLinesChannel(file)
 
-	for {
-		data := make([]byte, 8)
+	for line := range lines {
 
-		n, err := file.Read(data)
+		fmt.Printf("read: %s\n", line)
 
-		if err != nil {
-			fmt.Printf("read: %s\n", str)
-			break
-		}
-
-		if newLineIndex := bytes.IndexByte(data, '\n'); newLineIndex != -1 {
-			str += string(data[:newLineIndex])
-			fmt.Printf("read: %s\n", str)
-			str = string(data[newLineIndex+1:])
-		} else {
-			str += string(data[:n])
-		}
-
-		// data = data[:n]
-
-		// if i := bytes.IndexByte(data, '\n'); i != 0 {
-		// 	str += string(data[:i])
-		// 	data = data[i+1:]
-		// 	fmt.Printf("read: %s\n", str)
-		// 	str = ""
-		// }
-
-		// fmt.Printf("read: %s\n", string(data[:n]))
 	}
 
 }
