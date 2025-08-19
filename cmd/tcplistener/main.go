58da496c00e747e3998f3adc69a43ad3bb34476d
akhand08 <adib08@outlook.com>
2025-08-19 23:43:35 +0600

    testing directory setup

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -8,12 +8,12 @@ import (
 	"net"
 )
 
-func getLinesChannel(file io.ReadCloser) <-chan string {
+func getLinesChannel(conn io.ReadCloser) <-chan string {
 	out := make(chan string, 1)
 
 	go func() {
 
-		defer file.Close()
+		defer conn.Close()
 		defer close(out)
 
 		str := ""
@@ -21,14 +21,9 @@ func getLinesChannel(file io.ReadCloser) <-chan string {
 		for {
 			data := make([]byte, 8)
 
-			n, err := file.Read(data)
+			n, err := conn.Read(data)
 
 			if err != nil {
-
-				if len(str) > 0 {
-					out <- str
-					out <- "end"
-				}
 
 				if err != io.EOF {
 					log.Fatal("error: ", err)
@@ -37,15 +32,22 @@ func getLinesChannel(file io.ReadCloser) <-chan string {
 				break
 			}
 
+			data = data[:n]
 			if newLineIndex := bytes.IndexByte(data, '\n'); newLineIndex != -1 {
 				str += string(data[:newLineIndex])
 				// fmt.Printf("read: %s\n", str)
+				data = data[newLineIndex+1:]
 				out <- str
-				str = string(data[newLineIndex+1:])
-			} else {
-				str += string(data[:n])
+				str = ""
+
 			}
 
+			str += string(data)
+
+		}
+
+		if len(str) != 0 {
+			out <- str
 		}
 
 	}()
@@ -69,7 +71,9 @@ func main() {
 
 		for line := range getLinesChannel(connection) {
 
-			fmt.Printf("read: %s\n", line)
+			fmt.Printf("%s\n", line)
+			// fmt.Printf("%T\n", line)
+			// fmt.Println(string(line[0]), " --> ", line[1], " ---> ", line[2])
 
 		}
 	}
