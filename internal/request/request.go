84535f892e4a8e4a62ed4a91b6bfc176e7391adf
akhand08 <adib08@outlook.com>
2025-08-25 12:44:31 +0600

    streamed http reques line parsed successfully

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,13 +4,23 @@ import (
 	"bytes"
 	"errors"
 	"io"
-	"log"
 	"strings"
 	"unicode"
 )
 
+const bufferSize = 4096
+const chunkSize = 8
+
+type State int
+
+const (
+	Initialized State = iota
+	Done
+)
+
 type Request struct {
 	RequestLine RequestLine
+	State       State
 }
 
 type RequestLine struct {
@@ -19,27 +29,60 @@ type RequestLine struct {
 	RequestTarget string
 }
 
+func (r *Request) parse(buffer []byte) (n int, err error) {
+
+	newLineIndex := 0
+
+	if newLineIndex = bytes.Index(buffer, []byte("\r\n")); newLineIndex == -1 {
+		return 0, nil
+	}
+
+	line := buffer[:newLineIndex]
+	newRequestLine, err := parseRequestLine(string(line))
+
+	if err != nil {
+		return 0, err
+	}
+
+	r.RequestLine = *newRequestLine
+	r.State = Done
+
+	return len(line) + 2, nil
+
+}
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		log.Fatal("cannot read error: ", err)
+	buffer := make([]byte, bufferSize, bufferSize)
+	readToIndex := 0
+	newRequest := &Request{State: Initialized}
+
+	for newRequest.State != Done {
+
+		chunk := make([]byte, 8)
+
+		n, err := reader.Read(chunk)
+
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buffer[readToIndex:], chunk)
+		readToIndex += n
+
+		consumed, err := newRequest.parse(buffer)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if consumed > 0 {
+
+			readToIndex -= consumed
+			buffer = buffer[consumed+1:]
+		}
+
 	}
-
-	requestLine := ""
-
-	if newLineIndex := bytes.Index(data, []byte("\r\n")); newLineIndex != -1 {
-
-		requestLine = string(data[:newLineIndex])
-
-	}
-	newRequestLine, err := parseRequestLine(requestLine)
-
-	if err != nil {
-		return nil, err
-	}
-
-	newRequest := &Request{RequestLine: *newRequestLine}
 
 	return newRequest, nil
 
@@ -60,6 +103,10 @@ func parseRequestLine(requestLine string) (*RequestLine, error) {
 		if !unicode.IsUpper(letr) {
 			return nil, errors.New("Method is not in uppercase")
 		}
+	}
+
+	if httpVersion != "1.1" {
+		return nil, errors.New("Invalid http version")
 	}
 
 	newRequestLine := &RequestLine{HttpVersion: httpVersion, Method: method, RequestTarget: requestTarget}
